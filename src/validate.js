7239c43b0b4ee7468331ebc1f4a9581682aba3b6
duxq <951092973@qq.com>
2020-02-12 17:15:15 +0800

    feat: checkPlatform

--- src/validate.js
+++ src/validate.js
@@ -1,3 +1,28 @@
 export const isMail = mail => /^([a-zA-Z]|[0-9])(\w|-)+@[a-zA-Z0-9]+\.([a-zA-Z]{2,4})$/.test(mail)
 
 export const isPhone = phone => /^1[3456789]\d{9}$/.test(phone)
+
+export const checkPlatform = () => {
+
+    if(/android/i.test(navigator.userAgent)){
+  
+        return 'android' //这是Android平台下浏览器
+    }
+  
+    if(/(iPhone|iPad|iPod|iOS)/i.test(navigator.userAgent)){
+  
+        return 'ios' //这是iOS平台下浏览器
+    }
+  
+    if(/Linux/i.test(navigator.userAgent)){
+  
+        return 'Linux-browser' //这是Linux平台下浏览器
+    }
+  
+    if(/MicroMessenger/i.test(navigator.userAgent)){
+  
+        return 'wexin-browser' // 微信浏览器
+    }
+  
+    return null
+}
